1b54a12ed2be5ece6063e6c0fa51bfec16b4e22c
yinm <yinm.cminor9@gmail.com>
2018-06-28 09:35:29 +0900

    inherits when used with `require("util").inherits

--- test/test.js
+++ test/test.js
@@ -12,4 +12,29 @@ describe('EventEmitter', () => {
     assume(EventEmitter.EventEmitter).equals(EventEmitter)
   })
 
+  it('inherits when used with `require("util").inherits', () => {
+    function Beast() {
+      EventEmitter.call(this)
+    }
+
+    require('util').inherits(Beast, EventEmitter)
+
+    let moop = new Beast()
+    let meap = new Beast()
+
+    assume(moop).is.instanceOf(Beast)
+    assume(moop).is.instanceOf(EventEmitter)
+
+    moop.listeners()
+    meap.listeners()
+
+    moop.on('data', () => {
+      throw new Error('I should not emit')
+    })
+
+    meap.emit('data', 'rawr')
+    meap.removeListener('foo')
+    meap.removeAllListeners()
+  })
+
 })
